517db4f7a27ea45bb3bd5452efb29072d6bfc02e
John <johnsaplad61@gmail.com>
2025-12-02 15:01:20 +0800

    CustomerResetPassword

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Notifications\CustomResetPassword;
 
 class User extends Authenticatable
 {
@@ -88,4 +89,8 @@ class User extends Authenticatable
     {
         return $this->role === 'customer';
     }
+    public function sendPasswordResetNotification($token)
+{
+    $this->notify(new CustomResetPassword($token));
+}
 }
